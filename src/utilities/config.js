2ae5daeefc82c908bcc6d5857dfd450a3065ba8e
ammarinnovative <126662033+ammarinnovative@users.noreply.github.com>
2023-09-28 15:27:52 +0500

    updated code

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -1,3 +1,3 @@
 export const baseURL = "https://findmybartender.thewebtestlink.xyz/api/";
-export const imageURL = "http://24.199.93.30:3000/";
+export const imageURL = "https://findmybartender.thewebtestlink.xyz/";
 export const socketUrl = "http://24.199.93.30:5000";
